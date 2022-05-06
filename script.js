6b3f6847934c5c4299127fa1f0223a330f850d8f
zone0119 <54246376+zone0119@users.noreply.github.com>
2022-05-06 17:11:12 +0600

    add: log

--- script.js
+++ script.js
@@ -4,8 +4,16 @@ console.log('script');
 import  {Keyboard}  from '../../assets/js/main.js';
 
 
-const keyboard1 = new Keyboard("RU");
-//console.log(keyboard1.language);
+const keyboard1 = new Keyboard("en");
+console.log(keyboard1.language);
 
 
-keyboard1.createContainer();
+window.addEventListener("load", function(event) {
+    console.log("All resources finished loading!");
+
+    keyboard1.createContainer();
+    keyboard1.focusOn();
+    keyboard1.listenEvent();
+    
+  });
+
